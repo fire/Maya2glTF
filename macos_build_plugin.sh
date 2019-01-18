91ed1e5d90d4ea6427083024a1a84c0b054f4634
Ziriax <bugfact@gmail.com>
2019-01-18 21:02:58 +0100

    Trying to get precompiled headers working, but can't seem to disable it for C files with CLANG?

--- macos_build_plugin.sh
+++ macos_build_plugin.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 mkdir -p build
+# sudo rm -r build
+# mkdir -p build
 cd build
-if [ -f ./CMakeCache.txt ]; then
-  rm ./CMakeCache.txt
-fi
+echo Deleting CMake output files...
+find . -iwholename '*cmake*' -maxdepth 1 -delete
 echo Generating Unix makefiles...
 /Applications/CMake.app/Contents/bin/cmake -G "Unix Makefiles" ..
+#-DCMAKE_BUILD_TYPE=Release -DCMAKE_RULE_MESSAGES:BOOL=OFF -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ..
 #echo Building plugin...
 #make
 echo Installing plugin...
